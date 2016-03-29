be6d2d357a53d3241961237aa8c28e07f11e238c
crysispht <810613371@qq.com>
2016-03-29 23:52:41 +0800

    添加了index主页

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: 81061
+     * Date: 2016/3/29 0029
+     * Time: 23:50
+     */
+echo "<h1>HEllO EVERYONE!!!</h1>";
--- info.php
+++ info.php
@@ -1,8 +1,8 @@
 <?php
-    /**
-     * Created by PhpStorm.
-     * User: Administrator
-     * Date: 2016/3/23
-     * Time: 下午 11:00
-     */
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/3/23
+ * Time: 下午 11:00
+ */
 phpinfo();
